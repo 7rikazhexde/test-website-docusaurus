3f19b36496a878bf4def2559b6a1237ab852ad90
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2024-03-18 21:05:55 +0900

    :sparkles:Changed i18n codes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,75 +1,37 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Translate from '@docusaurus/Translate'; // 追加
 import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: {
-      en: 'Easy to Use',
-      ja: '使いやすさ',
-    },
+    title: 'homepage.features.easy.title',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: {
-      en: (
-        <>
-          Docusaurus was designed from the ground up to be easily installed and
-          used to get your website up and running quickly.
-        </>
-      ),
-      ja: (
-        <>
-          Docusaurus は、ウェブサイトを簡単にインストールして立ち上げるために、ゼロから設計されました。
-        </>
-      ),
-    },
+    description: 'homepage.features.easy.description',
   },
   {
-    title: {
-      en: 'Focus on What Matters',
-      ja: '重要なことに集中',
-    },
+    title: 'homepage.features.focus.title',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: {
-      en: (
-        <>
-          Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-          ahead and move your docs into the <code>docs</code> directory.
-        </>
-      ),
-      ja: (
-        <>
-          Docusaurus は、あなたのドキュメントに焦点を当てることを可能にし、私たちが作業を行います。ドキュメントを <code>docs</code> ディレクトリに移動してください。
-        </>
-      ),
-    },
+    description: 'homepage.features.focus.description',
   },
   {
-    title: {
-      en: 'Powered by React',
-      ja: 'Reactで動作',
-    },
+    title: 'homepage.features.react.title',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: {
-      en: (
-        <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
-        </>
-      ),
-      ja: (
-        <>
-          React を再利用してウェブサイトのレイアウトを拡張またはカスタマイズします。Docusaurus は、同じヘッダーとフッターを再利用しながら拡張することができます。
-        </>
-      ),
-    },
+    description: 'homepage.features.react.description',
   },
 ];
 
-function Feature({Svg, title, description}) {
-  const { i18n: {currentLocale} } = useDocusaurusContext();
-  const localizedTitle = title[currentLocale] || title;
-  const localizedDescription = description[currentLocale] || description;
+function Feature({ Svg, title, description }) {
+  const translatedTitle = <Translate id={title} />;
+  const translatedDescription = (
+    <Translate
+      id={description}
+      values={{
+        docusaurus: <strong>Docusaurus</strong>,
+        docDir: <code>docs</code>,
+      }}
+    />
+  );
 
   return (
     <div className={clsx('col col--4')}>
@@ -77,8 +39,8 @@ function Feature({Svg, title, description}) {
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{localizedTitle}</Heading>
-        <p>{localizedDescription}</p>
+        <Heading as="h3">{translatedTitle}</Heading>
+        <p>{translatedDescription}</p>
       </div>
     </div>
   );

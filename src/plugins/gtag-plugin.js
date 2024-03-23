bd696aafea86cb16eb9cdef10be3c5bd7cf689f9
7rikazhexde <33836132+7rikazhexde@users.noreply.github.com>
2024-03-23 18:35:45 +0900

    Updated gtag plugin setting

--- src/plugins/gtag-plugin.js
+++ src/plugins/gtag-plugin.js
@@ -1,8 +1,10 @@
+/*
 const { Cookies } = require('react-cookie-consent');
 
 module.exports = function (context, options) {
   const isGtagEnabled = Cookies.get('allowGtag') === 'true';
   //console.log('isGtagEnabled',isGtagEnabled)
+  //console.log('trackingID',options.trackingID)
 
   return {
     name: 'custom-gtag-plugin',
@@ -28,6 +30,49 @@ module.exports = function (context, options) {
                 });
               `,
             },
+          ],
+        };
+      } else {
+        return {};
+      }
+    },
+  };
+};
+*/
+
+const { Cookies } = require('react-cookie-consent');
+
+module.exports = function(context, options) {
+  const isGtagEnabled = Cookies.get('allowGtag') === 'true';
+
+  return {
+    name: 'custom-gtag-plugin',
+    injectHtmlTags() {
+      const gtmScript = isGtagEnabled
+        ? {
+            tagName: 'script',
+            innerHTML: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.onload = function() {
+                gtag('js', new Date());
+                gtag('config', '${options.trackingID}', { anonymize_ip: ${options.anonymizeIP} });
+              }
+            `,
+          }
+        : null;
+
+      if (isGtagEnabled) {
+        return {
+          headTags: [
+            {
+              tagName: 'script',
+              attributes: {
+                async: true,
+                src: `https://www.googletagmanager.com/gtag/js?id=${options.trackingID}`,
+              },
+            },
+            gtmScript,
           ],
         };
       } else {

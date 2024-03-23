55969a4d477aca0c88706005794bbcf951e09ecd
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2024-03-23 21:03:04 +0900

    Fixed gtag plugin codes

--- src/plugins/gtag-plugin.js
+++ src/plugins/gtag-plugin.js
@@ -2,9 +2,9 @@ const { Cookies } = require('react-cookie-consent');
 const fs = require('fs');
 const path = require('path');
 
+// 本関数はビルド時(GitHub Actions)にindex.htmlにGoogle Analyticsのスクリプトを挿入する。
+// ページのリクエスト時に、ページが読み込まれるたびにブラウザがクッキーをチェックし、allowGtagがtrueに設定されている場合にのみGoogle Analyticsを有効にする。
 module.exports = function(context, options) {
-  const isGtagEnabled = Cookies.get('allowGtag') === 'true';
-
   return {
     name: 'custom-gtag-plugin',
     postBuild(props) {
@@ -16,14 +16,19 @@ module.exports = function(context, options) {
       const gtagScript = `<script async src="https://www.googletagmanager.com/gtag/js?id=${options.trackingID}"></script>`;
 
       // gtag関数の実行スクリプト
-      const gtmScript = isGtagEnabled ? `
+      const gtmScript = `
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${options.trackingID}', { anonymize_ip: ${options.anonymizeIP} });
+
+          // クッキーの値に基づいてGAの設定を行う
+          const isGtagEnabled = document.cookie.includes('allowGtag=true');
+          if (isGtagEnabled) {
+            gtag('config', '${options.trackingID}', { anonymize_ip: ${options.anonymizeIP} });
+          }
         </script>
-      ` : '';
+      `;
 
       htmlContent = htmlContent.replace('</head>', `${gtagScript}\n${gtmScript}</head>`);
       fs.writeFileSync(htmlFilePath, htmlContent);

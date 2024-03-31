7c3d43f19de620eeac66ced98dbb2d76520a8b78
7rikazhexde <33836132+7rikazhexde@users.noreply.github.com>
2024-03-31 17:55:13 +0900

    feat: Updated docusaurus-search-local settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,76 +4,69 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
-const isNetlifyProduction = process.env.NETLIFY && process.env.CONTEXT === 'production';
+const isNetlifyProduction =
+  process.env.NETLIFY && process.env.CONTEXT === "production";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Test Website Docusaurus',
-  tagline: 'Welcome to my website',
-  favicon: 'img/favicon.ico',
+  title: "Test Website Docusaurus",
+  tagline: "Welcome to my website",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: isNetlifyProduction ? 'https://{your-netlify-site-url}' : 'https://7rikazhexde.github.io',
+  url: isNetlifyProduction
+    ? "https://{your-netlify-site-url}"
+    : "https://7rikazhexde.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: isNetlifyProduction ? '/' : '/test-website-docusaurus/',
+  baseUrl: isNetlifyProduction ? "/" : "/test-website-docusaurus/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '7rikazhexde', // Usually your GitHub org/user name.
-  projectName: 'test-website-docusaurus', // Usually your repo name.
+  organizationName: "7rikazhexde", // Usually your GitHub org/user name.
+  projectName: "test-website-docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en','ja'],
+    defaultLocale: "en",
+    locales: ["en", "ja"],
   },
 
   customFields: {
     deploy: isNetlifyProduction,
   },
 
-  plugins: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
-        indexDocs: true,
-        language: ["en","ja"],
-      },
-    ],
-  ],
-
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-      //https://docusaurus.io/docs/using-plugins#docusauruspreset-classic
+        //https://docusaurus.io/docs/using-plugins#docusauruspreset-classic
         docs: {
           // docs/index.mdをTopページにする場合にコメントアウトを外すこと。
           //routeBasePath: '/',
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/7rikazhexde/test-website-docusaurus/edit/main',
+            "https://github.com/7rikazhexde/test-website-docusaurus/edit/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/7rikazhexde/test-website-docusaurus/edit/main',
+            "https://github.com/7rikazhexde/test-website-docusaurus/edit/main",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -83,70 +76,70 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Home',
+        title: "Home",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {type: 'localeDropdown',position: 'right'},
+          { to: "/blog", label: "Blog", position: "left" },
+          { type: "localeDropdown", position: "right" },
           {
-            to: '/tutorial',  
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            to: "/tutorial",
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            to: '/app',
-            type: 'docSidebar',
-            sidebarId: 'appSidebar',           
-            position: 'left',
-            label: 'App',
+            to: "/app",
+            type: "docSidebar",
+            sidebarId: "appSidebar",
+            position: "left",
+            label: "App",
           },
           {
-            to: '/sample',
-            type: 'docSidebar',
-            sidebarId: 'sampleSidebar',
-            position: 'left',
-            label: 'Sample',
+            to: "/sample",
+            type: "docSidebar",
+            sidebarId: "sampleSidebar",
+            position: "left",
+            label: "Sample",
           },
           {
-            href: 'https://github.com/7rikazhexde/test-website-docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/7rikazhexde/test-website-docusaurus",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
+                label: "Tutorial",
                 //src/pages/index.jsをTopページにする場合
-                to: '/docs/tutorial/intro',
+                to: "/docs/tutorial/intro",
                 //docs/index.mdをTopページにする場合
                 //src/pages/index.jsをsrc/pages/index.js0にすること。
                 //to: '/tutorial/intro',
               },
-              {  
-                label: 'App',
+              {
+                label: "App",
                 // src/pages/index.jsをTopページにする場合
-                to: '/docs/app/',
+                to: "/docs/app/",
                 // docs/index.mdをTopページにする場合
                 // src/pages/index.jsをsrc/pages/index.js0にすること。
                 //to: '/app',
               },
               {
-                label: 'Sample',
+                label: "Sample",
                 // src/pages/index.jsをTopページにする場合
-                to: '/docs/sample/',
+                to: "/docs/sample/",
                 // docs/index.mdをTopページにする場合
                 // src/pages/index.jsをsrc/pages/index.js0にすること。
                 //to: '/sample',
@@ -154,25 +147,25 @@ const config = {
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'X(Twitter)',
-                href: 'https://twitter.com/tw_7rikazhexde',
+                label: "X(Twitter)",
+                href: "https://twitter.com/tw_7rikazhexde",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'About',
+                label: "About",
                 // src/pages/index.jsをTopページにする場合
-                to: '/docs/about',
+                to: "/docs/about",
                 // docs/index.mdをTopページにする場合
                 // src/pages/index.jsをsrc/pages/index.js0にすること。
                 //to: '/about',
-              }
+              },
             ],
           },
         ],
@@ -183,6 +176,24 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // https://github.com/easyops-cn/docusaurus-search-local?tab=readme-ov-file#usage
+        // https://github.com/easyops-cn/docusaurus-search-local/issues/206
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        indexBlog: false,
+        // docs/index.mdをTopページにする場合にコメントアウトを外すこと。
+        //docsRouteBasePath: "/",
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+        language: ["en","ja"],
+      },
+    ],
+  ],
 };
 
 export default config;
